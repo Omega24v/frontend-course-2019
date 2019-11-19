fb4e0b38b5dec1ca512a5ba32dab229d0074b764
Anton <omega24v1993@gmail.com>
2019-11-19 23:56:28 +0200

    add webpack & fix end of line

---
+++ homeworks/anton.sergienko_omega24v/homework_js_2/src/bundle.js
@@ -0,0 +1,5 @@
+import './style.sass';
+
+(function () {
+    console.log('Hello Guys!');
+}());
---
+++ homeworks/anton.sergienko_omega24v/homework_js_2/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/bundle.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss|sass)$/,
+                use: ExtractTextPlugin.extract(
+                    {
+                        fallback: 'style-loader',
+                        use: ['css-loader', 'sass-loader'],
+                    },
+                ),
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new ExtractTextPlugin(
+            { filename: 'style.css' },
+        ),
+        new HtmlWebpackPlugin({
+            inject: false,
+            hash: false,
+            template: './src/index.html',
+            filename: 'index.html',
+        }),
+    ],
+};
